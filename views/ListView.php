4faf5d2f03d4dba9619c936f7eb902b5d34f71fa
Dave Thomas <dthomas@starkartthenala.com>
2013-07-17 15:20:00 -0700

    comment cleanup

--- views/ListView.php
+++ views/ListView.php
@@ -1,5 +1,21 @@
 <?php
 
+/**
+ * The idea here is that the controller will do the work (whatever that amounts
+ * to) and will instantiate a new View of the appropriate type. That view will
+ * have a render method which will render the page appropriately.
+ *
+ * In this way we have some separation of concerns:
+ *
+ * - the controller marshalls the data and passes it to a view
+ *
+ * - the view massages that data into the presentation form (using templates)
+ *
+ * The routes.php file decides which controller to use; the controller decides
+ * which view class to use; the view class decides which template(s) to use.
+ */
+
+
 class ListView
 {
 	private $data = null;
