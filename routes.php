3da4cfc1d140d39a7df17f6b01479aec3f6e2421
Dave Thomas <dthomas@starkartthenala.com>
2013-07-17 08:04:07 -0700

    added route for queries

--- routes.php
+++ routes.php
@@ -4,14 +4,36 @@
 
 $klein = new \Klein\Klein;
 
-$klein->respond('GET','/hello',function(){
-	return "Hello world!";
+// Handle the routing for queries
+//
+// a regular expression for parsing queries
+$query_regex = "^(/([a-zA-Z]+)/([0-9]+)){0,5}(/([a-zA-Z]+)(/[0-9]+)?)[/]?$";
+// set up the route
+$klein->respond('GET',"@{$query_regex}",function($request,$response){
+
+	$args = explode('/', $request->uri());
+
+	$args = array_filter($args);
+
+	Kint::dump($args);
 });
 
+
+
+
+
+
+
+
+
+// Handle a 404 - route not found
+//
 $klein->respond('404', function($request){
 	$r = "<h1>Uh-oh. 404!</h1>";
 	$r .= "<p>The path '".$request->uri()."' does not exist.</p>";
 	return $r;
 });
-$klein->dispatch();
 
+// Execute!
+//
+$klein->dispatch();
