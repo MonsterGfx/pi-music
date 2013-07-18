777eeac8f80a7b6ebc2ff5b11f6280d52418faa0
Dave Thomas <dthomas@starkartthenala.com>
2013-07-18 13:34:22 -0700

    added Scan class

---
+++ libraries/Scan.php
@@ -0,0 +1,14 @@
+<?php
+
+class Scan {
+
+	public static function scanAll()
+	{
+		// get the file path
+		$path = Config::get('app.music-path');
+
+print_r($path);
+
+		// iterate through the folders
+	}
+}
