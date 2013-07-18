34ef86e67a8de02ccaea3cb28733948cb953cb12
Dave Thomas <info@monstergfx.com>
2013-07-17 20:51:21 -0700

    comment cleanup

--- public/index.php
+++ public/index.php
@@ -1,5 +1,9 @@
 <?php
 
+// include the autoload file, which will include any other required files
+//
 require_once __DIR__.'/../vendor/autoload.php';
 
+// start the route processor
+//
 require_once __DIR__.'/../routes.php';
