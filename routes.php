e256cd84477450152c6ed89d4f9c9af5ca8e3ee8
Dave Thomas <dave@monstergfx.com>
2013-07-17 21:03:43 -0700

    added test code for ID3 extraction

--- routes.php
+++ routes.php
@@ -24,7 +24,16 @@ $klein->respond('GET',"@{$query_regex}",function($request,$response){
 
 $klein->respond('GET','/test-route', function($request,$response){
 
-	Kint::dump(Config::get('app.sample'));
+	// attempt to use get_id3 to scan an MP3 file
+	$filename = '/media/music/Abba/The Albums/18 Dance (While The Music Still Goes.mp3';
+
+	$getID3 = new getID3;
+
+	// Analyze file and store returned data in $ThisFileInfo
+	$file_info = $getID3->analyze($filename);
+
+	return "<pre>".print_r($file_info,true)."</pre>";
+	// Kint::dump($file_info);
 
 });
 
