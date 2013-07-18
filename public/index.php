a088bd4735fd214c81b5b66a7ac31cd130ae1960
Dave Thomas <dave@monstergfx.com>
2013-07-17 21:30:53 -0700

    added try/catch block to index.php to catch any unhandled exceptions

--- public/index.php
+++ public/index.php
@@ -1,9 +1,18 @@
 <?php
 
-// include the autoload file, which will include any other required files
-//
-require_once __DIR__.'/../vendor/autoload.php';
+try {
 
-// start the route processor
-//
-require_once __DIR__.'/../routes.php';
+	// include the autoload file, which will include any other required files
+	//
+	require_once __DIR__.'/../vendor/autoload.php';
+
+	// start the route processor
+	//
+	require_once __DIR__.'/../routes.php';
+
+}
+catch(Exception $e) {
+	echo "<h1>Unhandled Exception!</h1>";
+
+	Kint::dump($e);
+}
