aee1be427ad5c846d1890a2343fce65354530f32
Dave Thomas <dthomas@starkartthenala.com>
2013-07-19 08:27:06 -0700

    added template path & template cache path to app config

--- config/app.php
+++ config/app.php
@@ -4,4 +4,8 @@ return array(
 
 	'music-path' => '/home/local/STARKART/dthomas/Music/',
 	// 'music-path' => '/media/music/Kate Bush/'
+
+	'template-path' => dirname(__FILE__).'/../views/templates/',
+
+	'template-cache-path' => dirname(__FILE__).'/../storage/cache/',
 );
