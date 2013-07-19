60237935964b8e0e8237c3068ae684b14e085a7d
Dave Thomas <dthomas@starkartthenala.com>
2013-07-19 13:48:27 -0700

    added query logging configuration

--- config/database.php
+++ config/database.php
@@ -6,4 +6,9 @@ return array(
 	 * The DSN to the SQLite3 Database
 	 */
 	'dsn' => 'sqlite:'.dirname(__FILE__).'/../database/musix.sqlite3',
+
+	/**
+	 * Turn on query logging
+	 */
+	'logging' => true,
 );
