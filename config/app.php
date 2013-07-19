9330fa82efc213761d3e2ebcb8c4b47f30227bac
Dave Thomas <dthomas@starkartthenala.com>
2013-07-19 07:47:26 -0700

    changed path for music library

--- config/app.php
+++ config/app.php
@@ -4,6 +4,6 @@ return array(
 
 	'sample' => 'My Sample Config Value',
 
-	// 'music-path' => '/home/local/STARKART/dthomas/Music/',
-	'music-path' => '/media/music/Kate Bush/'
+	'music-path' => '/home/local/STARKART/dthomas/Music/',
+	// 'music-path' => '/media/music/Kate Bush/'
 );
