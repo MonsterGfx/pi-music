f11cb68e01c01b7d7f7edfa1cbafbc954a6c3d77
Dave Thomas <dthomas@starkartthenala.com>
2013-07-19 14:18:50 -0700

    added method to retrieve album stats

--- models/Album.php
+++ models/Album.php
@@ -26,4 +26,31 @@ class Album extends Model {
 	{
 		return $this->has_many('Song');
 	}
+
+	/**
+	 * Get the statistics for display at the head of a list
+	 * 
+	 * @return array
+	 */
+	public function getStats()
+	{
+		// get the list of songs
+		$songs = $this->songs()->find_many();
+
+		// calculate the total time
+		$time = 0;
+		foreach($songs as $s)
+			$time += $s->playtime_seconds;
+		$time = round($time/60);
+
+		// return the results
+		return array(
+			'artist' => $this->artist()->find_one()->name,
+			'name' => $this->name,
+			'year' => $this->year ?: null,
+			'song_count' => count($songs) ?: null,
+			'total_time' => $time ?: null,
+		);
+
+	}
 }
