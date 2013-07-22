7166af67abe9b7876aa703bb8062363f935462df
Dave Thomas <info@monstergfx.com>
2013-07-21 21:13:42 -0700

    added addSong method to playist model

--- models/Playlist.php
+++ models/Playlist.php
@@ -17,6 +17,36 @@ class Playlist extends BaseModel {
 		return $this->has_many_through('Song');
 	}
 
+	/**
+	 * Add a song to the playlist
+	 *
+	 * @param type Song $song 
+	 * The song to add
+	 *
+	 * @param type $sort_order 
+	 * The position in the list in which to place it (default = the end)
+	 */
+	public function addSong(Song $song, $sort_order=999999)
+	{
+		// add the song to the playlist
+		$playlist_song = Model::factory('PlaylistSong')->create();
+
+		$playlist_song->playlists_id = $this->id;
+		$playlist_song->songs_id = $song->id;
+		$playlist_song->sort_order = $sort_order;
+		$playlist_song->save();
+
+		// now normalize the sort order
+		$playlist_song = Model::factory('PlaylistSong')->where('playlists_id',$this->id)->find_many();
+
+		for($i=0; $i<count($playlist_song); $i++)
+		{
+			$ps = $playlist_song[$i];
+			$ps->sort_order = $i*10;
+			$ps->save();
+		}
+	}
+
 	public function toArray()
 	{
 		return array(
