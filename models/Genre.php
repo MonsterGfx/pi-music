548c02280c72f7bf17b189bf15a5cb0c0067cf4d
Dave Thomas <dthomas@starkartthenala.com>
2013-07-30 15:12:04 -0700

    added getList method to genres

--- models/Genre.php
+++ models/Genre.php
@@ -1,4 +1,31 @@
 <?php
 
 class Genre {
+
+	/**
+	 * Get the list of genres
+	 * 
+	 * @return array
+	 */
+	public static function getList()
+	{
+		// get the list
+		$result = Music::send('list', 'genre');
+
+		// start building the list
+		$list = array();
+
+		// step through the results
+		foreach($result['values'] as $v)
+		{
+			if(substr($v,0,6)=='Genre:')
+				$list[] = trim(substr($v,6));
+		}
+
+		// sort the list
+		sort($list);
+
+		// and return it
+		return $list;
+	}
 }
