bb119a972ac344f39780536ce5c35d056e684286
Dave Thomas <info@monstergfx.com>
2013-07-29 21:23:52 -0700

    moved artist routes to separate file

---
+++ routes/artist.php
@@ -0,0 +1,58 @@
+<?php
+
+// Routes for artist requests
+//
+//		artist 					- list of artists
+//				page head='Artists', list head=null
+//
+//		artist/1/album			- list of albums for artist=1
+//				page head=artist.artist, list head=null
+//
+//		artist/1/album/2/song	- list of songs for artist=1, album=2
+//				page head=artist.artist, list head=album+stats
+//
+//		artist/1/album/2/song/3	- load all songs for artist=1, album=2, play song=3, go to nowplaying
+// 
+// 		artist/1/song			- list of all songs for artist=1
+//				page head=artist.artist, list head=null
+
+
+
+// artist 					- list of artists
+//
+$klein->respond('GET', '/artist', function($request, $response){
+	return "list of artists";
+
+});
+
+
+// artist/1/album			- list of albums for artist=1
+//
+$klein->respond('GET', '/artist/[:artist]/album', function($request, $response){
+	return "albums for artist: ".$request->param('artist');
+
+});
+
+
+// artist/1/album/2/song	- list of songs for artist=1, album=2
+//
+$klein->respond('GET', '/artist/[:artist]/album/[:album]/song$', function($request, $response){
+	return "songs for artist: ".$request->param('artist').", album: ".$request->param('album');
+
+});
+
+
+// artist/1/album/2/song/3	- load all songs for artist=1, album=2, play song=3, go to nowplaying
+//
+$klein->respond('GET', '/artist/[:artist]/album/[:album]/song/[i:song]$', function($request, $response){
+	return "play song ".$request->param('song')." for artist: ".$request->param('artist').", album: ".$request->param('album');
+
+});
+
+
+// artist/1/song			- list of all songs for artist=1
+//
+$klein->respond('GET', '/artist/[:artist]/song', function($request, $response){
+	return "songs for artist: ".$request->param('artist');
+
+});
