abc13a185262e0c63d8220fd2784b20c71b6d04b
Dave Thomas <dave@monstergfx.com>
2013-07-29 21:40:19 -0700

    added individual route files

---
+++ routes/now-playing.php
@@ -0,0 +1,37 @@
+<?php
+
+// The "Now Playing" routes
+
+// the "now playing" page
+//
+$klein->respond('GET','/', function($request){
+
+		// get the song info
+		$currentsong = Music::getCurrentSong();
+
+		// return the message
+		return NowPlayingPage::render($currentsong, $request);
+});
+
+// the "skip to previous song" action
+//
+$klein->respond('GET','/action-prev', function(){ Music::previous(); });
+
+// the "skip to next song" action
+//
+$klein->respond('GET','/action-next', function(){ Music::next(); });
+
+// the "toggle play/pause" action
+//
+$klein->respond('GET','/action-toggle-play', function(){ return Music::togglePlay(); });
+
+// the "adjust volume" action
+//
+$klein->respond('GET','/action-volume/[i:volume]', function($request){ Music::setVolume( $request->volume ); });
+
+// the "now playing update" request
+//
+$klein->respond('GET','/now-playing-update', function(){ return json_encode(Music::updateNowPlaying()); });
+
+
+
---
+++ routes/playlist.php
@@ -0,0 +1,38 @@
+<?php
+
+// Routes for playlist requests
+//		playlist 			- list of playlists
+//				page head='Playlist', list head=null
+//
+//		playlist/1/song			- list of songs for playlist=1
+//				page head=playlist.name, list head=null
+//
+//		playlist/1/song/2 	- load all songs for playlist=1, start playing song=2, go to nowplaying
+
+
+//		playlist 			- list of playlists
+//
+$klein->respond('GET', '/playlist', function($request, $response){
+	return "list of playlists";
+
+});
+
+
+//		playlist/1/song			- list of songs for playlist=1
+//
+$klein->respond('GET', '/playlist/[:playlist]/song', function($request, $response){
+	return "songs for playlist ".$request->param('playlist');
+
+});
+
+
+//		playlist/1/song/2 	- load all songs for playlist=1, start playing song=2, go to nowplaying
+//
+$klein->respond('GET', '/playlist/[:playlist]/song/[i:song]', function($request, $response){
+	return "play song ".$request->param('song')." for playlist ".$request->param('playlist');
+
+});
+
+
+
+
