55838db0d65d6aea444182425a100787184b3ebb
Dave Thomas <dthomas@starkartthenala.com>
2013-07-30 15:12:18 -0700

    added getArtists list to genres

--- models/Genre.php
+++ models/Genre.php
@@ -28,4 +28,38 @@ class Genre {
 		// and return it
 		return $list;
 	}
+
+	/**
+	 * Get the list of artists for the genre
+	 * 
+	 * @param string $genre 
+	 * The genre
+	 * 
+	 * @return array
+	 */
+	public static function getArtists($genre)
+	{
+		// query the MPD database
+		$result = Music::send('search', 'genre', $genre);
+
+		// get the list of songs
+		$songs = Music::buildSongList($result['values']);
+
+		// extract the album information from the list
+		$list = array();
+
+		foreach($songs as $s)
+		{
+			if(isset($s['Artist']))
+			{
+				$l = array(
+					'artist' => $s['Artist'],
+				);
+				if(!in_array($l, $list))
+					$list[] = $l;
+			}
+		}
+
+		return $list;
+	}
 }
