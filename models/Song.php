eb0320930fa0a61671ccef2ff1e265e2a999d25d
Dave Thomas <info@monstergfx.com>
2013-07-30 21:14:06 -0700

    added getList method to Song model

--- models/Song.php
+++ models/Song.php
@@ -2,4 +2,12 @@
 
 class Song {
 
+	public static function getList()
+	{
+		// get the list
+		$result = Music::send('listallinfo');
+
+		// return the values
+		return Music::buildSongList($result['values']);
+	}
 }
