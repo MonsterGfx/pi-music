8640f7628a590f1aad7199f9a990bca002f5cd22
Dave Thomas <info@monstergfx.com>
2013-07-31 14:57:34 -0700

    added getList method to playlist model

--- models/Playlist.php
+++ models/Playlist.php
@@ -1,14 +1,35 @@
 <?php
 
+class Playlist {
 
 	/**
+	 * Get the list of playlists
 	 * 
+	 * @return array
 	 */
+	public static function getList()
 	{
+		// send the command to MPD
+		$results = Music::send('listplaylists');
 
+		// start building a list
+		$list = array();
 
+		// step through the values returned by MPD
+		foreach($results['values'] as $r)
 		{
+			// is it a playlist line?
+			if(substr($r,0,9)=='playlist:')
+			{
+				// yes! extract the playlist name and add it to the list
+				$list[] = trim(substr($r,9));
+			}
 		}
 
+		// sort the list
+		sort($list);
+
+		// and return it
+		return $list;
 	}
 }
