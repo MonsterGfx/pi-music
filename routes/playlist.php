60cdff795c3a802c2458be584a83581b3ee34474
Dave Thomas <dave@monstergfx.com>
2013-07-31 15:03:47 -0700

    completed /playlist/1/song route

--- routes/playlist.php
+++ routes/playlist.php
@@ -34,8 +34,24 @@ $klein->respond('GET', '/playlist', function($request, $response){
 //		playlist/1/song			- list of songs for playlist=1
 //
 $klein->respond('GET', '/playlist/[:playlist]/song', function($request, $response){
-	return "songs for playlist ".$request->param('playlist');
+	// get the params
+	$playlist = Music::decode($request->param('playlist'));
 
+	// get the list of songs
+	$list = Playlist::getSongs($playlist);
+
+	// walk the array and construct URLs
+	// The encoded URL value is actually "artist name|album title". The artist
+	// name is included to ensure that albums with the same name are not
+	// conflated and the pipe character is a delimiter
+	array_walk($list, function(&$v, $k) use ($playlist) {
+		$v = array(
+			'name' => $v['Title'],
+			'url' => '/playlist/'.Music::encode($playlist).'/song/'.Music::encode($v['file']),
+		);
+	});
+
+	return ListPage::render($playlist, null, null, $list);
 });
 
 
