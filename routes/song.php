34252a13b9775a9e01c63361f06d8274978007f9
Dave Thomas <info@monstergfx.com>
2013-07-30 21:38:17 -0700

    added description

--- routes/song.php
+++ routes/song.php
@@ -41,7 +41,7 @@ $klein->respond('GET', '/song', function($request, $response){
 //		song/1 	- load ALL songs, play song=1, go to nowplaying
 //
 $klein->respond('GET', '/song/[:song]', function($request, $response){
-	return "start playing song ".$request->param('song');
+	return "start playing song ".Music::decode($request->param('song'));
 
 });
 
