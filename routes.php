8dbee94548f92f713f8fc58bf41b5470fefc6133
Dave Thomas <dthomas@starkartthenala.com>
2013-08-02 15:13:46 -0700

    code cleanup

--- routes.php
+++ routes.php
@@ -2,42 +2,42 @@
 
 /**
  * The router for the application
- * 
+ *
  * This looks like a good spot to write some TODOs
- * 
+ *
  * * list page: fix toolbar at bottom
  * * list page: fix toolbar at top
  * * list page: add "back" button in upper left
  * * list page: add buttons to bottom toolbar
  * * list page: render album details when appropriate
- * 
+ *
  * * ******************** bump version 0.0.2-alpha
- * 
+ *
  * * implement playlists
- * 
+ *
  * * ******************** bump version 0.0.3-alpha
- * 
+ *
  * * scan music: remove missing files
- * 
+ *
  * * ******************** bump version 0.0.4-alpha
- * 
+ *
  * * album artwork
- * 
+ *
  * * ******************** bump version 0.0.5-alpha
- * 
+ *
  * * query caching system
- * 
+ *
  * * ******************** bump version 0.0.6-alpha
- * 
+ *
  * * install MPD
  * * implement MPD interface
- * 
+ *
  * * ******************** bump version 0.0.7-alpha
- * 
+ *
  * * bugfix: correct song order in query
- * 
+ *
  * * ******************** bump version 0.0.8-alpha
- * 
+ *
  * * implement "now playing" page
  * * redirect from query page to now playing page
  * * add "now playing" button to list page
@@ -45,49 +45,49 @@
  * * add control buttons to now playing page
  * * add volume control to now playing page
  * * toggle play/pause icons on now playing page
- * 
+ *
  * * ******************** bump version 0.0.9-alpha
- * 
+ *
  * * bugfix: songs not added in expected order
- * 
+ *
  * * ******************** bump version 0.0.10-alpha
- * 
+ *
  * * add "shuffle" buttons to song lists
- * 
+ *
  * * ******************** bump version 0.0.11-alpha
- * 
+ *
  * * add custom play/pause icons to play button
- * 
+ *
  * * ******************** bump version 0.0.12-alpha
- * 
+ *
  * * add update functionality to now-playing page
- * 
+ *
  * * ******************** bump version 0.0.13-alpha
- * 
+ *
  * * make "now playing" the default route
- * 
+ *
  * * ******************** bump version 0.0.14-alpha
- * 
+ *
  * ------------------------- this will get us to a point where the player works!
  * 
  * @todo refactor query route handling to simplify it
  * @todo correct/clarify "back" route (button)
  * 
  * @todo ******************** bump version 0.0.15-alpha
- * 
+ *
  * @todo playlist editor
- * 
+ *
  * @todo ******************** bump version 0.0.16-alpha
- * 
+ *
  * @todo testing
- * 
+ *
  * @todo ******************** bump version 0.1.0-beta
- * 
+ *
  * ------------------------- future enhancements
- * 
+ *
  * @todo add scrubbing control to now playing page
  * @todo desktop pc layout
- * 
+ *
  */
 
 use \PHPMPDClient\MPD as MPD;
@@ -210,7 +210,7 @@ $klein->respond('GET','/test-route', function($request,$response){
 	//				page head=artist.artist, list head=album+stats
 	//
 	//		artist/1/album/2/song/3	- load all songs for artist=1, album=2, play song=3, go to nowplaying
-	// 
+	//
 	// 		artist/1/song			- list of all songs for artist=1
 	//				page head=artist.artist, list head=null
 	//
