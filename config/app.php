08b60eff9610ad390223acd3a496a127d931618a
Dave Thomas <dave@monstergfx.com>
2013-08-04 14:53:55 -0700

    added comments to application config entries

--- config/app.php
+++ config/app.php
@@ -2,17 +2,41 @@
 
 return array(
 
-	'music-path' => '/home/local/STARKART/dthomas/Music/',
+	/**
+	 * The path to the music files 
+	 */
+	'music-path' => '/media/music/',
 
+	/**
+	 * The path to the folder where artwork will be saved
+	 */
 	'music-artwork-path'  => dirname(__FILE__).'/../storage/artwork/',
 
+	/**
+	 * The path to templates
+	 */
 	'template-path' => dirname(__FILE__).'/../views/templates/',
 
+	/**
+	 * The path to the template cache
+	 */
 	'template-cache-path' => dirname(__FILE__).'/../storage/cache/',
 
+	/**
+	 * The MPD connection string
+	 */
 	'mpd-connection' => 'unix:///var/run/mpd/socket',
 
+	/**
+	 * Should the system cache queries?
+	 * 
+	 * This is set to false while developing. It should be set to true in a
+	 * production system.
+	 */
 	'query-caching' => false,
 
+	/**
+	 * The query cache storage location
+	 */
 	'query-cache-path' => dirname(__FILE__).'/../storage/cache/',
 );
